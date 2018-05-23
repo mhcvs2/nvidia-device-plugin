d1e617d6789fb3fe444e4c234428fc88b045e121
mhcvs2 <merzhong0501@163.com>
2018-05-23 13:03:17 +0800

    add

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-docker build --build-arg http_proxy=http://109.105.4.17:3128 --build-arg https_proxy=http://109.105.4.17:8119 -t nvidia/k8s-device-plugin:1.10 .
+docker build --build-arg http_proxy=http://109.105.4.17:3128 --build-arg https_proxy=http://109.105.4.17:8119 \
+-t registry.bst-1.cns.bstjpc.com:5000/nvidia/k8s-device-plugin-env-debug:1.10 . \
+&& docker push registry.bst-1.cns.bstjpc.com:5000/nvidia/k8s-device-plugin-env-debug:1.10
